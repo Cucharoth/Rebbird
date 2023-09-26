6fc883362fe9d85bfa88ce3391f165fab8b8a24e
Cucharoth <109435900+Cucharoth@users.noreply.github.com>
2023-09-26 17:23:31 -0300

    First commit

---
+++ src/main/java/com/ufro/Rebbird/RebbirdApplication.java
@@ -0,0 +1,13 @@
+package com.ufro.Rebbird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebbirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebbirdApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ufro/Rebbird/model/user.java
@@ -0,0 +1,17 @@
+@Entity
+public class User {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "id", nullable = false)
+private Long id;
+
+@Column(name = "username", nullable = false, unique = true, length = 50)
+private String username;
+
+@Column(name = "password", nullable = false, length = 70)
+private String password;
+
+@Column(name = "profile_picture", columnDefinition="TEXT")
+private String profilePicture;
+public User() {}
