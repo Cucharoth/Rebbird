a52f6f47ce75bb2b73329bf33af809385e9ee0cb
Cucharoth <cucharoth@gmail.com>
2023-09-27 00:03:25 -0300

    27-09

--- src/main/java/com/ufro/Rebbird/model/user.java
+++ src/main/java/com/ufro/Rebbird/model/user.java
@@ -1,17 +1,21 @@
+package com.ufro.Rebbird.model;
+
+import jakarta.persistence.*;
+
 @Entity
 public class User {
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-@Column(name = "id", nullable = false)
-private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
-@Column(name = "username", nullable = false, unique = true, length = 50)
-private String username;
+    @Column(name = "username", nullable = false, unique = true, length = 50)
+    private String username;
 
-@Column(name = "password", nullable = false, length = 70)
-private String password;
+    @Column(name = "password", nullable = false, length = 70)
+    private String password;
 
-@Column(name = "profile_picture", columnDefinition="TEXT")
-private String profilePicture;
-public User() {}
+    @Column(name = "profile_picture", columnDefinition = "TEXT")
+    private String profilePicture;
+}
